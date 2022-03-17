c4a76e913fac0880ad6bd21924a7ed51ebe1d19e
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-17 02:47:37 -0400

    06-Styling using Css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import './App.css';
 import Navbar from './Navbar'
 import Home from './Home'
 
 // This is JSX Template
 function App() {
 
-  // Numbers and Strings are allowed but booleans and objects are not
-  const msg = "Welcome to the dogo-blog"
-  const link = "https://www.google.com/"
   return (
     <div className="App">
       <Navbar />
