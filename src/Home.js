b3c0a35d3b0433f40f555af1d2efd00003cfa86c
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-17 03:31:54 -0400

    10- looping the list using template

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     
-    // using useState hook, we can change the value of a
-    // variable in run time and it will be reactive, meaning that it will 
-    // get rendered again where ever it is being used
-    const [myName, setMyName] = useState("mario")
-
-    const handelClick = () => {
-        setMyName("Shivansh")
-    }
-
+    // blogs.map iterates through each blog in blogs
+    // key is mandatory, this way react know about which element is being changed
+    // then we build a template that applies for each element in loop
     return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{myName}</p>
-            <button onClick={handelClick}>Click me</button>
+        <div className="home">      
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))
+            }
         </div>
       );
 }
