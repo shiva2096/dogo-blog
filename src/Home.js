7ad04a5010e275cff7711e2fcdd27de2e2f3bb00
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-17 02:58:50 -0400

    07- Click Event Handling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    
+    const handelClick = (e) => {
+        console.log("Hello ninjas", e)
+    }
+    
+    const handelClickAgain = (name, e) => {
+        console.log("Hello " + name , e.target)
+    }
+    
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handelClick}>Click me</button>
+            <button onClick={ (e) => handelClickAgain("mario",e)}>Click me Again</button>
         </div>
       );
 }
