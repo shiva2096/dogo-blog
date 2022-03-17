baae84f8a2c2946a5e556f1da6ae0d688ce300a0
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-17 01:41:28 -0400

    basics of jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 
 // This is JSX Template
 function App() {
+
+  // Numbers and Strings are allowed but booleans and objects are not
+  const msg = "Welcome to the dogo-blog"
+  const link = "https://www.google.com/"
   return (
     <div className="App">
       <div className="content">
         <h1>
           App Content
         </h1>
+
+        <p>{msg}</p>
+        <p>{10}</p>
+        <p>{"Hello World"}</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google Home</a>
       </div>
     </div>
   );
