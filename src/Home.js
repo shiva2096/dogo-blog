8673342af3647d149af2b5a00ecbf4d91c9b1595
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-22 15:05:19 -0400

    15 - useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,10 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    
+    // Another state variable
+    const [name, setName] = useState("mario");
+
     // Function that filters based on id 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,15 +18,19 @@ const Home = () => {
     }
 
     // Use Effect hook runs every time the page is rendered
+    // Use effect will only run when the dependency (name) changes
+    // That is it will run when the page loads and then only when name chages
     useEffect(() => {
         console.log("use Effect hook ran");
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">      
             <BlogList blogs={blogs} title="All Blogs!!" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog) => blog.author==='mario')} title="Mario's Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName("Shivansh")}>change name</button>
+            <p>{name}</p>
         </div>
       );
 }
