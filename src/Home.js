fb0e6c29ec94d9439d9ca98e8b627e75ff19b822
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-03-22 16:16:01 -0400

    18- Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -3,24 +3,29 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
     // Use Effect hook runs every time the page is rendered
     // Use effect will only run when the dependency (name) changes
     // That is it will run when the page loads and then only when name chages
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data)
-        });
+        // setting a 1sec delay to see the Loading message
+        setTimeout(()=> {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+            });
+        }, 1000)
 
     }, []);
     
     //blogs added with <BlogList because it will only render after blogs have some data. 
     return (
         <div className="home">      
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!!" /> }
         </div>
       );
