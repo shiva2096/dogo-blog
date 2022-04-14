9c9b6c8d138926633458a0c39a3ffb115cd0cffa
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-04-14 00:55:55 -0400

    Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Navbar from './Navbar'
 import Home from './Home'
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 // This is JSX Template
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
