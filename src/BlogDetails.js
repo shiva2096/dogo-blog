6d288e5e08c38003bf32f4e6c3f49d991d861bdc
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-04-14 04:14:25 -0400

    displaying detailed blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -2,15 +2,29 @@
 // we can fetch parameters as a part of the link
 // like here we are using /blogs/id
 
+// After that we are using our custom hook to fetch data for that id
+
+
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
    
     const {id} = useParams();
+    const {data:blog, isPending, error } = useFetch('http://localhost:8000/blogs/'+ id);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+            
         </div>
       );
 }
