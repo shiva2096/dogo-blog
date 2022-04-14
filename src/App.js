6bfd4e3a10f04bd9a4e1d801ad768d526e3a2001
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-04-14 00:58:31 -0400

    multiple routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar'
 import Home from './Home'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Create from './Create';
 // This is JSX Template
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
           </Routes>
         </div>
       </div>
